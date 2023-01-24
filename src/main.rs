64ff3c166c856b90f4aa930dfa88b877093addf0
BK brandon.kurtz@airbnb.com <>
2023-01-23 18:40:14 -0800

    enum GuessResult

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,26 @@ use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
 
-fn process_guess(guess: u32, answer: u32) -> &'static str {
+enum GuessResult {
+    Small, 
+    Big,
+    Win,
+}
+
+fn process_guess(guess: u32, answer: u32) -> GuessResult {
     match guess.cmp(&answer) {
-        Ordering::Less => return "Too small",
-        Ordering::Greater => return "Too big",
-        Ordering::Equal => return "You win!"
+        Ordering::Less => return GuessResult::Small,
+        Ordering::Greater => return GuessResult::Big,
+        Ordering::Equal => return GuessResult::Win
     }
 }
 
 fn main() {
     let secret = rand::thread_rng().gen_range(1..100);
     println!("{secret}");
+    println!("Guess the number!");
     
     loop {
-        println!("Guess the number!");
         println!("Please input your guess.");
 
         let mut your_guess = String::new();
@@ -32,9 +38,13 @@ fn main() {
         };
 
         let g_result = process_guess(guess, secret);
-        println!("{}", g_result);
-        if g_result == "You win!" {
+        if matches!(g_result, GuessResult::Win) {
+            println!("You win!");
             break;
+        } else if matches!(g_result, GuessResult::Small) {
+            println!("Too low! Guess again!")
+        } else {
+            println!("Too high! Guess again!")
         }
         
         
